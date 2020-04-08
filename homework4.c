e3d5e94f81614b75e6c53ea15b1a6dd4353890a8
Anna Yu <annayu94@vt.edu>
2020-04-08 09:58:48 -0400

    Updated HW #4

--- homework4.c
+++ homework4.c
@@ -1,37 +1,65 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "homework4.h"
 
+
 int main(void)
 {
     char rChar;
     char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+	bool finished = false;
 
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
-
+	eUSCI_UART_Config uartConfig = {
+         EUSCI_A_UART_CLOCKSOURCE_SMCLK,               // SMCLK Clock Source = 3MHz
+         19,                                           // UCBR   = 312
+         8,                                            // UCBRF  = 8
+         0x55,                                         // UCBRS  = 0x55
+         EUSCI_A_UART_NO_PARITY,                       // No Parity
+         EUSCI_A_UART_LSB_FIRST,                       // LSB First
+         EUSCI_A_UART_ONE_STOP_BIT,                    // One stop bit
+         EUSCI_A_UART_MODE,                            // UART mode
+         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION // Oversampling
+    };
 
     // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
+	GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
+	GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1,
+	        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     // TODO: Initialize EUSCI_A0
-
+	UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
     // TODO: Enable EUSCI_A0
-
+	UART_enableModule(EUSCI_A0_BASE);
 
     while(1)
     {
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
+        if (UART_getInterruptStatus (EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+                == EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+        {
+            rChar = UART_receiveData(EUSCI_A0_BASE);
+        }
+        else
+        {
+            rChar = 0xFF;
+        }
 
 
         // TODO: If an actual character was received, echo the character to the terminal AND use it to update the FSM.
-        //       Check the transmit interrupt flag prior to transmitting the character.
+        //       Check the transmit interrupt flagx prior to transmitting the character.
+        if (rChar != 0xFF)
+        {
+            charFSM(rChar);
+        }
 
 
 
@@ -39,6 +67,9 @@ int main(void)
         //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
         //       Make sure to reset the success variable after transmission.
 
+        if (UART_getInterruptStatus (EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+                            == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+                        UART_transmitData(EUSCI_A0_BASE, rChar);
 
     }
 }
@@ -52,7 +83,19 @@ void initBoard()
 bool charFSM(char rChar)
 {
     bool finished = false;
-
-
+    switch(PS)
+    {
+    case S:
+        break;
+    case S2:
+        break;
+    case S25:
+        break;
+    case S253:
+        break;
+    case S2534:
+        finished = true;
+        break;
+    }
     return finished;
 }
